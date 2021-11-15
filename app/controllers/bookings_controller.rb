aff97f25afb2f62d147211311e59ca0ec1182061
thibaut butaye <thibautbutaye@gmail.com>
2021-11-15 17:48:43 +0100

    start controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,11 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.list = @list
+    flash[:notice] = @booking
+  end
 end
