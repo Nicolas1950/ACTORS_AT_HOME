fe7dd85414e8c7772737cced4485e49811c44b3c
adonis goushchi <amidonis@hotmail.com>
2021-11-15 17:43:08 +0100

    actor controller

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,45 @@
 class ActorsController < ApplicationController
+before_action :find, only:[:show, :edit, :update, :destroy]
+  def index
+    @actors = Actor.all
+  end
+
+  def new
+    @actor = Actor.new
+  end
+
+  def show
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+
+  end
+
+  def update
+
+    @task.update(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+  def destroy
+
+    @task.delete
+    redirect_to tasks_path
+
+  end
+  private
+
+  def find
+    @task = Task.find(params[:id])
+  end
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
 end
