0d591606aea6cf99d127f8ebcbf69243c3791251
adonis goushchi <amidonis@hotmail.com>
2021-11-15 16:15:46 +0100

    changing the migration of users

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_11_15_144852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "actors", force: :cascade do |t|
+    t.string "name"
+    t.float "price"
+    t.string "field"
+    t.text "description"
+    t.boolean "available"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_actors_on_user_id"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "actor_id", null: false
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["actor_id"], name: "index_bookings_on_actor_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "actors", "users"
+  add_foreign_key "bookings", "actors"
+  add_foreign_key "bookings", "users"
 end
