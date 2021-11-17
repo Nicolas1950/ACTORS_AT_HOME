447cbc879a179209cbdd79a713dc81100fc956ef
Filip Brouwers <filip.brouwers@gmail.com>
2021-11-17 15:26:41 +0100

    temp commit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,12 +8,24 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
-    @review.booking =
-    @
+    @actor = Actor.find(params[:actor_id])
+    @user = current_user
+    @booking = Booking.where(actor: @actor, user: @user)
+    @review.booking = @booking
+    @review.save
+    redirect_to actor_path(@actor)
   end
 
   def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
   end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:)
+
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'reviews/destroy'
   devise_for :users
   root to: 'pages#home'
   resources :actors do
-    resources :bookings, only: [:new, :create] do
-      resources :reviews, only: [:new, :create]
-    end
+    resources :bookings, only: [:new, :create]
   end
+  resources :bookings, only: [:new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :bookings, only: [:destroy]
+  resources :reviews, only: [:destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
