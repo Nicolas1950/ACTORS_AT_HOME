6d82445fd52f50280e157f0cecc9e9208a8071fc
Filip Brouwers <filip.brouwers@gmail.com>
2021-11-18 18:06:33 +0100

    dashboard start of ccs

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,5 @@ Rails.application.routes.draw do
   resources :bookings, only: [:destroy]
   resources :reviews, only: [:destroy]
   get 'dashboard', to: "pages#dashboard"
-<<<<<<< HEAD
-
-=======
->>>>>>> master
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
