295e5b1f590000937cd40cb3be1602557dc222ec
adonis goushchi <amidonis@hotmail.com>
2021-11-18 14:33:58 +0100

    map show the markers and i have 100 of them

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+require 'open-uri'
+require 'json'
 
 Actor.destroy_all
 User.destroy_all
 puts "Database cleaned"
 
 nicolas = User.create(email: "nicolas@hotmail.com", password: "123456")
-
-30.times do
-  Actor.create!(
+adonis = Actor.create(
       name: Faker::Artist.name,
       price: Faker::Number.number,
       field: "Comedy",
-      address: Faker::Address.full_address,
+      address: "Tour Eiffel",
       user: nicolas
     )
+
+url = "https://raw.githubusercontent.com/EthanRBrown/rrad/master/addresses-us-100.json"
+json = JSON.parse(URI.open(url).read)
+result = json["addresses"]
+result.each do |hash|
+  address = "#{hash["address1"]}, #{hash["city"]}"
+  actor = Actor.create!(
+    name: Faker::Artist.name,
+    price: Faker::Number.number,
+    field: "Comedy",
+    address: address,
+    user: nicolas
+  )
+  p actor
 end
 
 puts "Actors and users created"
