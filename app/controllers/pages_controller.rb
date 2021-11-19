5b16db9f3f77f70d3d96186bddf915a4985ad4fe
adonis goushchi <amidonis@hotmail.com>
2021-11-19 13:56:38 +0100

    reviews not finished

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,16 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
-
   def home
   end
 
   def dashboard
     @user = current_user
     @bookings_current_user = Booking.where(user: @user)
+  end
 
+  def new
+    @booking = Booking.new
+    @review = Review.new
   end
 end
